a3a51ae384ca7faf921ea464a2d5c15448c8f57a
이진호 <jinholee@nts-corp.com>
2020-01-10 10:57:25 +0900

    Router 및 Lazy import 적용 Home, Loader 컴포넌트 제작, esLint 및 prettier 설치

--- realworld-frontends-react/src/App.js
+++ realworld-frontends-react/src/App.js
@@ -1,10 +1,23 @@
-import React, {Component} from "react"
+import React, { Component, lazy, Suspense } from "react";
+import { Router, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+import Loader from "./common/Loader";
+
+const Home = lazy(() => import("./routes/Home"));
+
+const history = createBrowserHistory();
 
 class App extends Component {
-    render () {
-        return (
-            <div>Hello world</div>
-        )
-    }
+	render() {
+		return (
+			<Router history={history}>
+				<Suspense fallback={<Loader />}>
+					<Route exact path="/" component={Home} />
+					<Route path="/Home" component={Home} />
+				</Suspense>
+			</Router>
+		);
+	}
 }
 export default App;
