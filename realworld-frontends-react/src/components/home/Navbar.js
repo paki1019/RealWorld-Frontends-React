a26adf00ede6be06e52dcbcd09c6cdb6ff004a26
이진호 <jinholee@nts-corp.com>
2020-01-14 10:48:43 +0900

    Banner Navbar 컴포넌트 html 구현 차후 css 추가

---
+++ realworld-frontends-react/src/components/home/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+	render() {
+		return (
+			<nav className="navbar navbar-light">
+				<div className="container">
+					<Link to={"#"} className="navbar-brand">
+						conduit
+					</Link>
+					<ul className="nav navbar-nav pull-xs-right">
+						<li className="nav-item">
+							<Link to={"/"} className="nav-link">
+								Home
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to={"login"} className="nav-link">
+								Sign in
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link to={"register"} className="nav-link">
+								Sign up
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		);
+	}
+}
+
+export default Navbar;
--- realworld-frontends-react/src/routes/Home.js
+++ realworld-frontends-react/src/routes/Home.js
@@ -1,11 +1,24 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import Navbar from "../components/home/Navbar";
+import Banner from "../components/home/Banner";
 
 class Home extends Component {
-    render () {
-        return (
-            <div>Home</div>
-        )
-    }
+	render() {
+		return (
+			<>
+				<Navbar />
+				<div className="home-page">
+					<Banner />
+					<div className="container page">
+						<div className="row">
+							<div className="col-md-9"></div>
+							<div className="col-md-3"></div>
+						</div>
+					</div>
+				</div>
+			</>
+		);
+	}
 }
 
 export default Home;
